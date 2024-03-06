78f411466028c10e0ad5f80e33c5a5e49174a7f7
DavidingPlus <2065666169@qq.com>
2024-03-06 16:02:08 +0800

    fix api_key

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ client = OpenAI(
     # note： 没有使用 OpenAI 官方的 API，那个要给钱
     # 参考 github 开源项目：https://github.com/chatanywhere/GPT_API_free
     # 这是我的免费的 API KEY
-    api_key="sk-ilHiwmIb9kBoQdmcH0uOMWyGdsI1K33OaxbypjfuhvMgG6l7",
+    api_key=api_key,
     base_url="https://api.chatanywhere.tech/v1"
 )
 
@@ -48,7 +48,7 @@ def gpt_35_api_stream(model: str, messages: list):
 
 if __name__ == '__main__':
     model = "gpt-3.5-turbo"
-    messages = [{'role': 'user', 'content': '阐述一下中国历史的发展过程'},]
+    messages = [{'role': 'user', 'content': '请详细阐述勒布朗·詹姆斯的NBA生涯历程。'},]
     # 非流式调用
     # gpt_35_api(model, messages)
     # 流式调用
