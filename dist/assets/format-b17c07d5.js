e09db0b1eb7d75e69e025c10d0076bfa7175beec
李航宇 <1731978669@qq.com>
2024-04-06 16:29:44 +0800

    update code

---
+++ dist/assets/format-b17c07d5.js
@@ -0,0 +1,1 @@
+import{ao as t,ap as e}from"./index-a9c24727.js";const n=o=>t.get("/core/show-result/once",{params:{file_name:o}}),r=o=>t.get("/core/show-history/total",{params:o}),c=o=>{const a={file_name:o};t.delete("/core/delete-history/once",{data:a},{headers:{"Content-Type":"application/json"}})},l=o=>e(o).format("YYYY年MM月DD日 HH:mm:ss");export{n as a,c as d,l as f,r as g};
