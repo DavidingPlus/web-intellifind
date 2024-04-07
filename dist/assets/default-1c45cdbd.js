4e0984924678a5f2603ef309c58bc9e8c9206762
DavidingPlus <2065666169@qq.com>
2024-04-07 19:33:52 +0800

    remove dist

--- dist/assets/default-1c45cdbd.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/default-fb87342e.png";export{s as _};
--- dist/assets/format-a2d930ed.js
+++
@@ -1,1 +0,0 @@
-import{ao as t,ap as e}from"./index-bed6c77c.js";const n=o=>t.get("/core/show-result/once",{params:{file_name:o}}),r=o=>t.get("/core/show-history/total",{params:o}),c=o=>{const a={file_name:o};t.delete("/core/delete-history/once",{data:a},{headers:{"Content-Type":"application/json"}})},l=o=>e(o).format("YYYY年MM月DD日 HH:mm:ss");export{n as a,c as d,l as f,r as g};
